97766004dc6409c0a245a95487866e7967d128bc
Acacio de Lima <limadeacacio@gmail.com>
2025-03-21 10:14:50 -0300

    refactor: simplify loading state handling and enhance shop item display logic

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -18,14 +18,11 @@ export default function Shop() {
   const { shop: data, isLoadingShop, isFetchingShop, isErrorShop } = useShop();
   const countdown = useCountdown(21, 0); // 21:00 (9 PM)
 
-  // Determina se o conteúdo está em estado de carregamento
-  const isLoading = isLoadingShop || isFetchingShop;
-
   return (
     <div className="min-h-screen flex flex-col px-28 pt-12 pb-5">
       <ScrollToTopButton />
 
-      {isLoading || isErrorShop ? (
+      {isLoadingShop || isFetchingShop || isErrorShop ? (
         <ShopSkeleton />
       ) : (
         <header className="w-full">
@@ -44,27 +41,55 @@ export default function Shop() {
       )}
 
       <div className="flex-1 py-12">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {isLoading ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 space-y-12">
+          {(isLoadingShop && !data) || isFetchingShop || isErrorShop ? (
             <ShopCardSkeleton />
-          ) : (
-            <>
-              {data && data.shop.length > 0 ? (
-                data.shop.map((shop) => (
-                  <ShopCard key={shop.offerId} shop={shop} />
-                ))
-              ) : data && data.shop.length === 0 ? (
-                <div className="col-span-3 flex flex-col items-center justify-center py-10">
-                  <Image src={NotFound} alt="Not Found" className="w-auto h-48" />
+          ) : null}
 
-                  <div className="flex flex-col text-center space-y-2 mt-12">
-                    <span className="font-bold text-xl">
-                      Nenhum item encontrado na loja
+          {data &&
+            [
+              ...new Set(
+                data.shop.map((item) => item.section?.name || "Outros")
+              ),
+            ].map((section) => {
+              const items = data.shop.filter(
+                (shop) => (shop.section?.name || "Outros") === section
+              );
+
+              return (
+                <div key={section} className="col-span-full space-y-12">
+                  <div className="flex items-center gap-3">
+                    <div className="w-2 h-[1px] bg-gray-300/60 rounded-full" />
+                    <span className="border border-gray-300 text-black/80 rounded-md px-2 py-1 inline-block text-xl font-medium">
+                      {section.toUpperCase()}
                     </span>
+                    <div className="flex-1 h-[1px] bg-gray-300/60 rounded-full" />
+                  </div>
+
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 space-y-12">
+                    {items.map((shop) => (
+                      <ShopCard key={shop.offerId} shop={shop} />
+                    ))}
                   </div>
                 </div>
-              ) : null}
-            </>
+              );
+            })}
+
+          {data && data.shop.length === 0 && (
+            <div className="flex flex-col items-center justify-center py-10">
+              <Image src={NotFound} alt="Not Found" className="w-auto h-48" />
+
+              <div className="flex flex-col text-center space-y-2 mt-12">
+                <span className="font-bold text-xl">
+                  Nenhum item encontrado na loja
+                </span>
+
+                <small className="text-gray-500">
+                  Crie uma empresa clicando no botão 'Nova Empresa' para
+                  adicionar uma nova.
+                </small>
+              </div>
+            </div>
           )}
         </div>
       </div>
